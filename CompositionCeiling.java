33638d60351182bef3d61b8d9827166531cfbf64
Tr.yd <89679886+Tryshd@users.noreply.github.com>
2022-11-01 07:42:21 +0900

    Add files via upload

---
+++ CompositionCeiling.java
@@ -0,0 +1,14 @@
+public class CompositionCeiling{
+    private int height;
+    private int paintedColor;
+    public CompositionCeiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
