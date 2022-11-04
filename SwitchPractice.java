ac42b6c4dd1ce4499a8310d6e09c580bf7620860
Tr.yd <89679886+Tryshd@users.noreply.github.com>
2022-11-04 22:28:11 +0900

    Add files via upload

---
+++ SwitchPractice.java
@@ -0,0 +1,35 @@
+class SwitchPractice{
+    public static void main(String[] args){
+        int day = 10;
+        printDayofTheWeek(day);
+    }
+
+    public static void printDayofTheWeek(int day){
+            switch(day){
+                case 0:
+                System.out.println("Sunday");
+                break;
+                case 1:
+                System.out.println("Monday");
+                break;
+                case 2:
+                System.out.println("Tuesday");
+                break;
+                case 3:
+                System.out.println("Wednesday");
+                break;
+                case 4:
+                System.out.println("Tuesday");
+                break;
+                case 5:
+                System.out.println("Friday");
+                break;
+                case 6:
+                System.out.println("Saturday");
+                break;
+                default:
+                System.out.println("Invalid day");
+            }
+
+    }
+}
